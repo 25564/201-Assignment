7f2c6fb81e67609651fb5e8f694aef8113154439
Austin <awoldes@gmail.com>
2016-10-03 13:18:15 +1000

    Inital forms created

--- GroupProject/GroupProject/Form1.Designer.cs
+++ GroupProject/GroupProject/Form1.Designer.cs
@@ -23,12 +23,118 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.CoinGame = new System.Windows.Forms.RadioButton();
+            this.DiceGame = new System.Windows.Forms.RadioButton();
+            this.CardGame = new System.Windows.Forms.RadioButton();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monaco Arena";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CardGame);
+            this.groupBox1.Controls.Add(this.DiceGame);
+            this.groupBox1.Controls.Add(this.CoinGame);
+            this.groupBox1.Location = new System.Drawing.Point(68, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 179);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Game Type";
+            // 
+            // CoinGame
+            // 
+            this.CoinGame.AutoSize = true;
+            this.CoinGame.Location = new System.Drawing.Point(63, 44);
+            this.CoinGame.Name = "CoinGame";
+            this.CoinGame.Size = new System.Drawing.Size(77, 17);
+            this.CoinGame.TabIndex = 0;
+            this.CoinGame.TabStop = true;
+            this.CoinGame.Text = "Coin Game";
+            this.CoinGame.UseVisualStyleBackColor = true;
+            // 
+            // DiceGame
+            // 
+            this.DiceGame.AutoSize = true;
+            this.DiceGame.Location = new System.Drawing.Point(63, 77);
+            this.DiceGame.Name = "DiceGame";
+            this.DiceGame.Size = new System.Drawing.Size(78, 17);
+            this.DiceGame.TabIndex = 1;
+            this.DiceGame.TabStop = true;
+            this.DiceGame.Text = "Dice Game";
+            this.DiceGame.UseVisualStyleBackColor = true;
+            // 
+            // CardGame
+            // 
+            this.CardGame.AutoSize = true;
+            this.CardGame.Location = new System.Drawing.Point(63, 112);
+            this.CardGame.Name = "CardGame";
+            this.CardGame.Size = new System.Drawing.Size(78, 17);
+            this.CardGame.TabIndex = 2;
+            this.CardGame.TabStop = true;
+            this.CardGame.Text = "Card Game";
+            this.CardGame.UseVisualStyleBackColor = true;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(111, 362);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(122, 23);
+            this.ExitBtn.TabIndex = 2;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(111, 310);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(122, 23);
+            this.StartBtn.TabIndex = 3;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 423);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton CardGame;
+        private System.Windows.Forms.RadioButton DiceGame;
+        private System.Windows.Forms.RadioButton CoinGame;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Button StartBtn;
     }
 }
 
---
+++ GroupProject/GroupProject/Which_Card_Game.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GroupProject {
+    partial class Which_Card_Game {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ToMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a Game to Play";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Twenty-one",
+            "Crazy Eights"});
+            this.comboBox1.Location = new System.Drawing.Point(73, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // ToMenu
+            // 
+            this.ToMenu.Location = new System.Drawing.Point(57, 163);
+            this.ToMenu.Name = "ToMenu";
+            this.ToMenu.Size = new System.Drawing.Size(156, 40);
+            this.ToMenu.TabIndex = 2;
+            this.ToMenu.Text = "Exit";
+            this.ToMenu.UseVisualStyleBackColor = true;
+            // 
+            // Which_Card_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.ToMenu);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Which_Card_Game";
+            this.Text = "Which_Card_Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ToMenu;
+    }
+}
